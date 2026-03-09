bb5bfdd0356918104603be4c54f089c2f7924aca
Yanliang24 <yanliang.chen24@gmail.com>
2026-03-09 13:00:02 -0400

    Update evaluation.m

--- 03_metrics/evaluation.m
+++ 03_metrics/evaluation.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% evaluation - The code is to perform all evaluation tests
+% 
+% This script is part of the reproducibility package for the paper:
+% Chen, Y, Srivastava, A., and Park, C., Statistical Emulations of Human
+% Operational Motions in Industrial Environments
+%
+% Copyright ©2026 Yanliang Chen
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function Results = evaluation(X, Xnew, tree, KernelVMF,posturemode)
     
     Results = zeros(1,11);
@@ -40,5 +50,6 @@ end
     
     
     
+
 
 
