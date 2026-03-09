19a94caf879c80015a488965fa8cb4aedff55ac3
Yanliang24 <yanliang.chen24@gmail.com>
2026-03-09 12:13:01 -0400

    Update InverseExp_At_Posture.m

--- 02_functions/InverseExp_At_Posture.m
+++ 02_functions/InverseExp_At_Posture.m
@@ -1,3 +1,15 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% InverseExp_At_Posture - The code is to generate figure 4 and
+% figure 5
+% 
+% This script is part of the reproducibility package for the paper:
+% Chen, Y, Srivastava, A., and Park, C., Statistical Emulations of Human
+% Operational Motions in Industrial Environments
+%
+% Copyright ©2026 Yanliang Chen
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function [V] = InverseExp_At_Posture(Posture, Posture1)
 
 c_theta = sum(Posture .* Posture1, 2);
@@ -6,7 +18,3 @@ theta = acos(c_theta);
 d = theta ./ sin(theta);
 d(theta == 0 | isnan(d)) = 1; 
 V = d .* (Posture1 - c_theta .* Posture);
-
-
-
- 
