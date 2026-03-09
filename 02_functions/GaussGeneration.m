3dd4ddda3a55d709b5ae58100be6749749a1a311
Yanliang24 <yanliang.chen24@gmail.com>
2026-03-09 12:10:12 -0400

    Update GaussGeneration.m

--- 02_functions/GaussGeneration.m
+++ 02_functions/GaussGeneration.m
@@ -1,3 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% GaussGeneration - The code is to generate Gaussian random variables from traning data S
+% 
+% This script is part of the reproducibility package for the paper:
+% Chen, Y, Srivastava, A., and Park, C., Statistical Emulations of Human
+% Operational Motions in Industrial Environments
+%
+% Copyright ©2026 Yanliang Chen
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function [Snew] = GaussGeneration(S,Rn,Indep)
 
 [~,D2,D1] = size(S);
@@ -5,7 +16,7 @@ SS = reshape(S,[],D1*D2);
 MS = mean(SS);
 CS = cov(SS);
 
-if Indep == 1
+if Indep == 1  %Independent Gaussian if 1
     CS = diag(diag(CS));
 end
 
@@ -15,4 +26,5 @@ for i = 1:Rn
     RS = reshape(RS,D2,D1);
     Snew(i,:,:) = RS;
 end
+
 
