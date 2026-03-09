70ad8caa4b6a1ef78634f9f033fb136618f8ac42
Yanliang24 <yanliang.chen24@gmail.com>
2026-03-09 12:58:35 -0400

    Update dist_seq_max_posture.m

--- 03_metrics/dist_seq_max_posture.m
+++ 03_metrics/dist_seq_max_posture.m
@@ -1,3 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% dist_seq_max_posture - The code is to compute max posture distance 
+% between two sequences posture_seq1 and posture_seq2
+% 
+% This script is part of the reproducibility package for the paper:
+% Chen, Y, Srivastava, A., and Park, C., Statistical Emulations of Human
+% Operational Motions in Industrial Environments
+%
+% Copyright ©2026 Yanliang Chen
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [d] = dist_seq_max_posture(posture_seq1, posture_seq2)
 
 T = size(posture_seq1, 2);
@@ -9,5 +20,6 @@ for i = 1:T
     ss = sign(ss).* min(abs(ss), 1);
     di(i) = sum(acos(ss));
 end
+
 
 d = max(di);
