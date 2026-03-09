24810ea220a86b60aad6d6123574e3307f26ec9c
Yanliang24 <yanliang.chen24@gmail.com>
2026-03-09 14:00:18 -0400

    Update quantization.m

--- 03_metrics/quantization.m
+++ 03_metrics/quantization.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% quantization - The code is to compute quantization
+% 
+% This script is part of the reproducibility package for the paper:
+% Chen, Y, Srivastava, A., and Park, C., Statistical Emulations of Human
+% Operational Motions in Industrial Environments
+%
+% Copyright ©2026 Yanliang Chen
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [Y] = quantization(X, posturemode)
 
 [~,T,~] = size(X);
