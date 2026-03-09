e0f5c877491ebcd1a93bde8f79445ef1f79eef11
Yanliang24 <yanliang.chen24@gmail.com>
2026-03-09 16:11:41 -0400

    Update DrawSkeletonSequenceAction.m

--- 02_functions/DrawSkeletonSequenceAction.m
+++ 02_functions/DrawSkeletonSequenceAction.m
@@ -1,3 +1,17 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% --- SOURCE & ATTRIBUTION ---
+% Original Author: Park, C.
+% Provided by: Park, C.
+% 
+% Reference:Park, C., Noh, S.D. and Srivastava, A.
+%           Data science for motion and time analysis with modern motion sensor data. 
+%           Operations Research. 
+%
+% --- USAGE IN THIS STUDY ---
+% This function is slightly modified for formatting and used to visulize motion sequences (Figure 5, 6, 11, and 12)
+% in the paper: "Statistical Emulations of Human Operational Motions in Industrial Environments" (2026).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function h=DrawSkeletonSequenceAction(skeleton_bis,k,Ec,Lc,fs,varargin)
     %skeleton_bis contains the skeleton
     if nargin > 5,
