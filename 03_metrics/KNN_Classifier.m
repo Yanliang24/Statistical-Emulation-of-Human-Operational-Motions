7fefb4a5a1bde060e54fe8f3416c16e56b1d6385
Yanliang24 <yanliang.chen24@gmail.com>
2026-03-09 12:53:57 -0400

    Update KNN_Classifier.m

--- 03_metrics/KNN_Classifier.m
+++ 03_metrics/KNN_Classifier.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% KNN_Classifier - The code is to perform knn classification on X and Xnew 
+% 
+% This script is part of the reproducibility package for the paper:
+% Chen, Y, Srivastava, A., and Park, C., Statistical Emulations of Human
+% Operational Motions in Industrial Environments
+%
+% Copyright ©2026 Yanliang Chen
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [L] = KNN_Classifier(X, Xnew, Label, k)
     
 N = length(X);
@@ -7,3 +17,4 @@ end
 
 [~, inx] = mink(L0,k);
 L = mode(Label(inx));
+
