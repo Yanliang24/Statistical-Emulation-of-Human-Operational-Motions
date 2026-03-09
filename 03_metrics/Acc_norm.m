dc0e76b63f01e5eb9d7aef27da60a9d13b8da8a1
Yanliang24 <yanliang.chen24@gmail.com>
2026-03-09 12:41:38 -0400

    Update Acc_norm.m

--- 03_metrics/Acc_norm.m
+++ 03_metrics/Acc_norm.m
@@ -1,8 +1,20 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Acc_norm - The code is to compute norm of acceleration
+% 
+% This script is part of the reproducibility package for the paper:
+% Chen, Y, Srivastava, A., and Park, C., Statistical Emulations of Human
+% Operational Motions in Industrial Environments
+%
+% Copyright ©2026 Yanliang Chen
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function E = Acc_norm(A)
 
 T = size(A,1);
 dt = 1/(T-1);
 A_squared = sum(A.^2, 3);
 E = sum(A_squared, 'all')*dt;
+
 
 end
