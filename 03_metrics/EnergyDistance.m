b44b5114b555334ccb49032f68be9f410a6a6a1e
Yanliang24 <yanliang.chen24@gmail.com>
2026-03-09 12:43:43 -0400

    Update EnergyDistance.m

--- 03_metrics/EnergyDistance.m
+++ 03_metrics/EnergyDistance.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% EnergyDistance - The code is to compute energy distance between two datasets
+% 
+% This script is part of the reproducibility package for the paper:
+% Chen, Y, Srivastava, A., and Park, C., Statistical Emulations of Human
+% Operational Motions in Industrial Environments
+%
+% Copyright ©2026 Yanliang Chen
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [T0, D] = EnergyDistance(X, Xnew) 
 
 Xcom = [X Xnew];
@@ -17,4 +27,5 @@ end
 Na = M;
 Nb = Rn;
 T0 = (2*sum(D(1:Na,Na+1:Na+Nb),"all")/(Na*Nb)-sum(D(1:Na,1:Na),"all")/(Na*Na)-sum(D(Na+1:Na+Nb,Na+1:Na+Nb),"all")/(Nb*Nb)); 
+
     
