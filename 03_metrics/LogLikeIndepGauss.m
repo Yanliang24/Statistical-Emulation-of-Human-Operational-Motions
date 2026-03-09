b7fb9c679bd063dc860c283c0bbff0edc474880b
Yanliang24 <yanliang.chen24@gmail.com>
2026-03-09 12:56:54 -0400

    Update LogLikeIndepGauss.m

--- 03_metrics/LogLikeIndepGauss.m
+++ 03_metrics/LogLikeIndepGauss.m
@@ -1,3 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% LogLikeIndepGauss - The code is to compute log-likelihood of coefficient 
+% S using independent Gaussian distribution with mean 0 and variance Vf
+% 
+% This script is part of the reproducibility package for the paper:
+% Chen, Y, Srivastava, A., and Park, C., Statistical Emulations of Human
+% Operational Motions in Industrial Environments
+%
+% Copyright ©2026 Yanliang Chen
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [L] = LogLikeIndepGauss(S,Vf)
 
 [M,D2,D1] = size(S);
@@ -10,4 +21,5 @@ for i = 1:M
         LR = LR+lr;
     end
     L(i) = LR;
+
 end
