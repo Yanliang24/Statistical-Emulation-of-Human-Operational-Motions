7a32ee9ef3c45d858bfc2f0827c52d30d8100804
Yanliang24 <yanliang.chen24@gmail.com>
2026-03-09 12:24:38 -0400

    Update SpatialPCA.m

--- 02_functions/SpatialPCA.m
+++ 02_functions/SpatialPCA.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SpatialPCA - The code is to perform spatial PCA on IS-TVF or SIEM functions
+% 
+% This script is part of the reproducibility package for the paper:
+% Chen, Y, Srivastava, A., and Park, C., Statistical Emulations of Human
+% Operational Motions in Industrial Environments
+%
+% Copyright ©2026 Yanliang Chen
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [Z,Mu,Ud,Sigma] = SpatialPCA(X,d)
     
     [Ty,M,D] = size(X);
@@ -9,3 +19,4 @@ function [Z,Mu,Ud,Sigma] = SpatialPCA(X,d)
     Ud = U(:,1:d);
     Z = (X-Mu)*Ud;
     Z = reshape(Z,[Ty,M,d]);
+
