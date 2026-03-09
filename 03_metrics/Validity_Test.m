ad7db362e15ba2cfa55a49495e3e1eb8217dcd96
Yanliang24 <yanliang.chen24@gmail.com>
2026-03-09 13:59:26 -0400

    Update Validity_Test.m

--- 03_metrics/Validity_Test.m
+++ 03_metrics/Validity_Test.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Validity_Test - The code is to compute perform test of posture validity score and integrity rate
+% 
+% This script is part of the reproducibility package for the paper:
+% Chen, Y, Srivastava, A., and Park, C., Statistical Emulations of Human
+% Operational Motions in Industrial Environments
+%
+% Copyright ©2026 Yanliang Chen
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [S, Integrity] = Validity_Test(X, KernelVMF, alpha, tree)
     I = length(X);
     for i = 1:I
