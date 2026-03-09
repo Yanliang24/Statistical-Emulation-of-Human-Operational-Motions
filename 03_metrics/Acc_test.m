b9d60f1abdb87c2620a7aa68f96df8f0625a412e
Yanliang24 <yanliang.chen24@gmail.com>
2026-03-09 12:42:27 -0400

    Update Acc_test.m

--- 03_metrics/Acc_test.m
+++ 03_metrics/Acc_test.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Acc_test - The code is to compute acceleration energy of dataset X
+% 
+% This script is part of the reproducibility package for the paper:
+% Chen, Y, Srivastava, A., and Park, C., Statistical Emulations of Human
+% Operational Motions in Industrial Environments
+%
+% Copyright ©2026 Yanliang Chen
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [S] = Acc_test(X)
 
 I = length(X);
@@ -5,4 +15,5 @@ for i = 1:I
     A{i} = Acceleration(X{i});
     Si(i) = Acc_norm(A{i});
 end
+
 S = mean(Si);
