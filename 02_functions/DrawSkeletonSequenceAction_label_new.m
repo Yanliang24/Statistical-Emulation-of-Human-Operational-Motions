95e6e9dcd55531dd2c887b606763b2b3f9e42094
Yanliang24 <yanliang.chen24@gmail.com>
2026-03-12 17:00:18 -0400

    Update DrawSkeletonSequenceAction_label_new.m

--- 02_functions/DrawSkeletonSequenceAction_label_new.m
+++ 02_functions/DrawSkeletonSequenceAction_label_new.m
@@ -1,15 +1,11 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % --- SOURCE & ATTRIBUTION ---
-% Original Author: Park, C.
-% Provided by: Park, C.
+% Original Author: Anonymous.
+% Provided by: Anonymous.
 % 
-% Reference:Park, C., Noh, S.D. and Srivastava, A.
-%           Data science for motion and time analysis with modern motion sensor data. 
-%           Operations Research. 
-%
 % --- USAGE IN THIS STUDY ---
 % This function is slightly modified for formatting and used to visulize motion sequences (Figure 5, 6, 11, and 12)
-% in the paper: "Statistical Emulations of Human Operational Motions in Industrial Environments" (2026).
+% in the paper.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function h=DrawSkeletonSequenceAction_label_new(skeleton_bis,k,Ec,Lc,fs,varargin)
